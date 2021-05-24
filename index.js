4a8a46f2bd86257c2917c4293d0601500e3d672b
Estela <estelajimero@users.noreply.github.com>
2021-05-24 13:08:37 +0200

    Final commit

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 let express = require("express"),
-    app = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
+    app = express();
+    http = require("http");
+    server = http.createServer(app);
     mongoose = require('mongoose');
 
+let methodOverride = require('method-override');
+
+//Connection to DB
+mongoose.connect('mongodb://localhost/agenda', function(err, res) {
+    if(err) {
+        console.log('ERROR: conectando a base de datos.' + err);
+    } 
+    app.listen(3000, function() {
+        console.log("Node server running on http://localhost:3000");
+    });
+})
+
+//Middelwares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride());
 
-var router = express.Router();
+//Import Models and controllers
+let contactoController = require('./controllers/contactoController');
+
+//Example route
+let router = express.Router();
 
 router.get('/', function(req, res) {
     res.send("Hello World!");
@@ -16,6 +33,17 @@ router.get('/', function(req, res) {
 
 app.use(router);
 
-app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
-});
+//API routes
+let contactosRouter = express.Router();
+
+contactosRouter.route('/contacto')
+    .get(contactoController.findAllContactos)
+    .post(contactoController.addContacto);
+
+contactosRouter.route('/contacto/:id')
+    .get(contactoController.findById)
+    .put(contactoController.updateContacto)
+    .delete(contactoController.deleteContacto);
+
+app.use('/', contactosRouter);
+
